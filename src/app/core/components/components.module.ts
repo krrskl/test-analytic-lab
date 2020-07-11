84fa4ba6922b0d430da0754b8287783f08135ce9
Rubén Carrascal <krrskl97@gmail.com>
2020-07-11 04:33:55 -0500

    :ok_hand: Showing the stats.

--- src/app/core/components/components.module.ts
+++ src/app/core/components/components.module.ts
@@ -2,11 +2,15 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MapComponent } from './map/map.component';
 
+/* Chart Js */
+import { ChartsModule } from 'ng2-charts';
+import { ChartComponent } from './chart/chart.component';
+
 @NgModule({
-  imports: [CommonModule],
-  exports: [MapComponent],
+  imports: [CommonModule, ChartsModule],
+  exports: [MapComponent, ChartComponent],
   providers: [],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  declarations: [MapComponent],
+  declarations: [MapComponent, ChartComponent],
 })
 export class ComponentsModule {}
